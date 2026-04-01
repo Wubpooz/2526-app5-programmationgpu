551e893648fdc3dc14ae7627836583570868b8c1
Wubpooz <mathieu.waharte@gmail.com>
2026-04-01 16:24:07 +0200

    TP2 w. notes

--- tp/tp2/matmul.cu
+++ tp/tp2/matmul.cu
@@ -6,69 +6,82 @@
 
 // A and C are stored by rows, i.e., A(i, j) = A[i * N + j], C(i, j) = C[i * N + j]
 // B is stored by columns, i.e., B(i, j) = B[i + j * N]
-// A et C sont stockes par lignes, a savoir A(i, j) = A[i * N + j], C(i, j) = C[i * N + j]
-// B est stocke par colonne, a savoir B(i, j) = B[i + j * N]
 float *A, *B, *C;
 
 // dA and dC are stored by rows, dB is stored by columns
-// dA et dC sont stockes par lignes, dB est stocke par colonne
 float *dA, *dB, *dC;
 
 
 // Create a block for computing each element C(i, j), compute using 1 thread by block
-// Creer un bloc pour le calcul de chaque element C(i, j), calculer avec 1 thread par bloc
 __global__ void multiplyMatrixGPUByBlocks(float *dA, float *dB, float *dC, int n)
 {
-  // TODO / A FAIRE ...
+  int i = blockIdx.x; // i increases by 1 for each block in the x dimension
+  int j = blockIdx.y; // j increases by 1 for each block in the y dimension
+  dC[i * n + j] = 0.0f;
+  for (int k = 0; k < n; k++) {
+    dC[i * n + j] += dA[i * n + k] * dB[k + j * n];
+  }
 }
 
 
 // Create a block for computing blockDim.x elements of C, compute using blockDim.x threads per block. Each thread computes one element of C
 // Assume N is a multiple of blockDim.x
-// Creer un bloc pour le calcul de blockDim.x elements de C, calculer avec blockDim.x threads par bloc. Chaque thread calcule un element de C.
-// Supposer que N est un divisible par blockDim.x
 __global__ void multiplyMatrixGPUByBlocksThreads1D(float *dA, float *dB, float *dC, int n)
 {
-  // TODO / A FAIRE ...
+  int i = blockIdx.x * blockDim.x + threadIdx.x; // i increases by 1 for each thread in a block, then by blockDim.x for each block
+  int j = blockIdx.y; // = 1
+  dC[i * n + j] = 0.0f;
+  for (int k = 0; k < n; k++) {
+    dC[i * n + j] += dA[i * n + k] * dB[k + j * n];
+  }
 }
 
 
 // Create a block for computing blockDim.x elements of C, compute using blockDim.x threads per block. Each thread computes one element of C
 // Make it work when N is not a multiple of blockDim.x
-// Creer un bloc pour le calcul de blockDim.x elements de C, calculer avec blockDim.x threads par bloc.
-// Chaque thread calcule un element de C.
-// Faire marcher pour N n'est pas multiple de blockDim.x.
 __global__ void multiplyMatrixGPUByBlocksThreads1DNonMultiple(float *dA, float *dB, float *dC, int n)
 {
-  // TODO / A FAIRE ...
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y;
+  if (i < n && j < n) {  // guard to avoid out-of-bounds access because N may not be a multiple of blockDim.x
+    dC[i * n + j] = 0.0f;
+    for (int k = 0; k < n; k++) {
+      dC[i * n + j] += dA[i * n + k] * dB[k + j * n];
+    }
+  }
 }
 
 
 // Create a block for computing blockDim.x * blockDim.y elements of C, compute using blockDim.x * blockDim.y threads per block.
 // Each thread computes one element of C.
 // Assume N is a multiple of blockDim.x
-// Creer un bloc pour le calcul de blockDim.x * blockDim.y elements de C, calculer avec blockDim.x * blockDim.y threads par bloc.
-// Chaque thread calcule un element de C.
-// Supposer que N est un divisible par blockDim.x
 __global__ void multiplyMatrixGPUByBlocksThreads2D(float *dA, float *dB, float *dC, int n)
 {
-  // TODO / A FAIRE ...
+  int i = blockIdx.x * blockDim.x + threadIdx.x; // i increases by 1 for each thread in a block, then by blockDim.x for each block in the x dimension
+  int j = blockIdx.y * blockDim.y + threadIdx.y; // j increases by 1 for each thread in a block, then by blockDim.y for each block in the y dimension
+  dC[i * n + j] = 0.0f;
+  for (int k = 0; k < n; k++) {
+    dC[i * n + j] += dA[i * n + k] * dB[k + j * n];
+  }
 }
 
 
 // Create a block for computing blockDim.x * blockDim.y elements of C, compute using blockDim.x * blockDim.y threads per block. Each thread computes one element of C
 // Make it work when N is not a multiple of blockDim.x nor blockDim.y
-// Creer un bloc pour le calcul de blockDim.x * blockDim.y elements de C, calculer avec blockDim.x * blockDim.y threads par bloc.
-// Chaque thread calcule un element de C.
-// Faire marcher pour N n'est pas multiple de ni blockDim.x ni blockDim.y.
 __global__ void multiplyMatrixGPUByBlocksThreads2DNonMultiple(float *dA, float *dB, float *dC, int n)
 {
-  // TODO / A FAIRE ...
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  if (i < n && j < n) { // guard to avoid out-of-bounds access because N may not be a multiple of blockDim.x nor blockDim.y
+    dC[i * n + j] = 0.0f;
+    for (int k = 0; k < n; k++) {
+      dC[i * n + j] += dA[i * n + k] * dB[k + j * n];
+    }
+  }
 }
 
 
 // Reference CPU code for multipying matrices C = AB (A, C stored by rows, B stored by columns)
-// Code reference de CPU pour effectuer la multiplication de matrices C = AB (A, C stockes par ligne, B stocke par colonne)
 void multiplyMatrixCPU()
 {
   for (int i = 0; i < N; i++) {
@@ -101,7 +114,6 @@ void verifyResults()
 int main(int argc, char **argv)
 {
   // Initialization
-  // Initialisation
   A = (float *)malloc(N * N * sizeof(A[0]));
   B = (float *)malloc(N * N * sizeof(B[0]));
   C = (float *)malloc(N * N * sizeof(C[0]));
@@ -113,57 +125,53 @@ int main(int argc, char **argv)
   }
 
   // Allocate dA and dB, then copy the arrays A and B to the GPU
-  // Allouer dA et dB, puis copier les tableaux A et B vers le GPU
-  // TODO / A FAIRE ...
+  cudaMalloc(&dA, sizeof(float) * N * N);
+  cudaMalloc(&dB, sizeof(float) * N * N);
+  cudaMalloc(&dC, sizeof(float) * N * N);
 
+  cudaMemcpy(dA, A, sizeof(float) * N * N, cudaMemcpyHostToDevice);
+  cudaMemcpy(dB, B, sizeof(float) * N * N, cudaMemcpyHostToDevice);
 
   // Call each GPU kernel appropriately to multiply matrices A and B
   // Measure and print the execution time and performance (GFlops/s) of each kernel, without counting the data transfer time
-  // Appeler chaque kernel GPU de maniere appropriee pour multiplier les matrices A et B
-  // Mesurer et afficher le temps d'execution et la performance (en GFlops/s) de chaque kernel, sans compter le temps de transfert.
-  // TODO / A FAIRE ...
   {
-    dim3 dimGrid;
-    dim3 dimBlock;
-    // multiplyMatrixGPUByBlocks<<<..., ...>>>(N);
+    dim3 dimGrid(N, N); // one block for each element of C
+    dim3 dimBlock; // one thread per block
+    multiplyMatrixGPUByBlocks<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
-    dim3 dimGrid;
-    dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads1D<<<..., ...>>>(N);
+    dim3 dimGrid(N);
+    dim3 dimBlock(N);
+    multiplyMatrixGPUByBlocksThreads1D<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   { 
-    dim3 dimGrid;
-    dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads1DNonMultiple<<<..., ...>>>(N);
+    dim3 dimGrid(N);
+    dim3 dimBlock(N);
+    multiplyMatrixGPUByBlocksThreads1DNonMultiple<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
-    dim3 dimGrid;
-    dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads2D<<<..., ...>>>(N);
+    dim3 dimGrid(N, N);
+    dim3 dimBlock(N, N);
+    multiplyMatrixGPUByBlocksThreads2D<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
   {
-    dim3 dimGrid;
-    dim3 dimBlock;
-    // multiplyMatrixGPUByBlocksThreads2DNonMultiple<<<..., ...>>>(N);
+    dim3 dimGrid(N, N);
+    dim3 dimBlock(N, N);
+    multiplyMatrixGPUByBlocksThreads2DNonMultiple<<<dimGrid, dimBlock>>>(dA, dB, dC, N);
   }
 
   // Copy the array dC back to the CPU
-  // Recopier le tableau dC vers le CPU
-  // TODO / A FAIRE ...
+  cudaMemcpy(C, dC, sizeof(float) * N * N, cudaMemcpyDeviceToHost);
 
   // Verify the results
-  // Verifier les resultats
-  // multiplyMatrixCPU();
+  multiplyMatrixCPU();
   verifyResults();
 
   // Deallocate A, B, C
-  // Desallouer A, B, C
   free(A); free(B); free(C);
 
   // Deallocate dA, dB, dC
-  // Desallouer dA, dB, dC
-  // TODO / A FAIRE ...
+  cudaFree(dA); cudaFree(dB); cudaFree(dC);
 
   return 0;
 }
