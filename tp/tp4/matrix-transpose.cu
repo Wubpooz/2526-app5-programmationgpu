5ba7b7a69677de7e5eadaca5f5ed9e3a591ac0a6
Wubpooz <mathieu.waharte@gmail.com>
2026-04-09 10:45:04 +0200

    remove french doc

--- tp/tp4/matrix-transpose.cu
+++ tp/tp4/matrix-transpose.cu
@@ -1,9 +1,3 @@
-/**
-  * Kernel 1: Implement a first kernel where each block (using BSXY x BSXY threads) transposes a BSXY x BSXY tile of A, and writes it into the corresponding location in At. Do without using shared memory.
-  *
-  * Kernel 2: In the second kernel, do the same, but using the shared memory. Each block should load a tile of BSXY x BSXY of A into the shared memory, then perform the transposition using this tile in the shared memory into At. Test the difference in speedup. Test the performance using shared memory without padding and with padding (to avoid shared memory bank conflicts).
-  *
-  */
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
@@ -13,6 +7,10 @@
 #include <cfloat>
 
 #define BSXY 32
+/**
+  * Kernel 1: Implement a first kernel where each block (using BSXY x BSXY threads) transposes a BSXY x BSXY tile of A, and writes it into the corresponding location in At. Do without using shared memory.
+  * Kernel 2: In the second kernel, do the same, but using the shared memory. Each block should load a tile of BSXY x BSXY of A into the shared memory, then perform the transposition using this tile in the shared memory into At. Test the difference in speedup. Test the performance using shared memory without padding and with padding (to avoid shared memory bank conflicts).
+  */
 
 void transposeCPU(float *A, float *At, int N)
 {
